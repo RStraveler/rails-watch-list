af93e816dd14702cccfaa94f0f97cc577e0fee88
Rogerio DaSilva <rogerioa.farinha@gmail.com>
2021-08-13 16:31:48 +0200

    watch-movies final

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -22,6 +22,12 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to list_path
+  end
+
   private
 
   def params_list
